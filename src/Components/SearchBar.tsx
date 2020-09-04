30734e5ad0d006ff861403e844528858a583e952
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2020-09-04 12:28:59 +0700

    Push after make home and list responsive.

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import React, { FormEvent } from 'react';
 import logo from "../Assets/logo-512-filmku.png";
-import { DropdownProps, Input, Select } from "semantic-ui-react";
+import { DropdownProps, Input } from "semantic-ui-react";
 import Button from "semantic-ui-react/dist/commonjs/elements/Button";
 import Icon from "semantic-ui-react/dist/commonjs/elements/Icon";
 import styles from './SearchBar.module.css';
@@ -21,7 +21,7 @@ const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
     <div className={styles.SearchBarContainer}>
       <img src={logo} alt="logo-filmku" className={styles.Logo} onClick={props.handleLogoClick}/>
       <Input fluid className={styles.InputSearch} type='text' placeholder='Search movie...' action>
-        <input  onChange={props.handleSearchInputChange} onKeyDown={props.handleEnterKeyDown} value={props.query} />
+        <input onChange={props.handleSearchInputChange} onKeyDown={props.handleEnterKeyDown} value={props.query}/>
         {/*<Select fluid*/}
         {/*        clearable*/}
         {/*        options={props.yearRange}*/}
@@ -30,7 +30,7 @@ const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
         {/*        className={styles.SelectYear}*/}
         {/*        onChange={props.handleYearPickChange}*/}
         {/*/>*/}
-        <Button type='submit' icon onClick={props.handleButtonSearchClick}><Icon name='search' /></Button>
+        <Button type='submit' icon onClick={props.handleButtonSearchClick}><Icon name='search'/></Button>
       </Input>
     </div>
   )
