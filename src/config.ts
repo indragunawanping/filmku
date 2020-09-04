1619eca6219cc0b6ac8636a0b52597e96737792d
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2020-09-04 19:22:21 +0700

    Minor refactoring.

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
-export const REACT_APP_APPLICATION_BASE_URL = process.env.REACT_APP_APPLICATION_BASE_URL;
-export const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+// export const REACT_APP_APPLICATION_BASE_URL = process.env.REACT_APP_APPLICATION_BASE_URL;
+// export const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+export const REACT_APP_APPLICATION_BASE_URL = 'https://www.omdbapi.com/';
+export const REACT_APP_API_KEY = 'faf7e5bb';
